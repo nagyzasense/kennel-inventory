035060d2ca0f5af1096ac6905944d1527cdc59a7
Zsolt Nagy <zsolt.nagy@bredex.de>
2026-01-26 21:23:45 +0100

    Create application

---
+++ src/main/java/com/nagyzasense/kennel/KennelInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.nagyzasense.kennel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KennelInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KennelInventoryApplication.class, args);
+	}
+
+}
